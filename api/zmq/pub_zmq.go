cc2f31d84282bd0e6966685dd9610a6b6a05794d
textolytics <textolytics@gmail.com>
2021-02-13 18:01:50 -0500

    fefef

--- api/zmq/pub_zmq.go
+++ api/zmq/pub_zmq.go
@@ -13,7 +13,7 @@ func PubZmq() {
 	//  Prepare our publisher
 	publisher, _ := zmq.NewSocket(zmq.Type(zmq.PUB))
 	defer publisher.Close()
-	publisher.Bind("tcp://*:5563")
+	publisher.Bind("tcp://*:5500")
 	for {
 		//  Write two messages, each with an envelope and content
 		publisher.Send("A", zmq.Flag(zmq.SNDMORE))
