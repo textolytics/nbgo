9c7bd2cd65f073bb14248fc9e07f18cc18a3af3b
textolytics <textolytics@gmail.com>
2021-02-14 08:50:17 -0500

    hbjnm

--- shortener/model.go
+++ shortener/model.go
@@ -1,7 +1,6 @@
 package shortener
 
-
-
+// Redirect struct {
 type Redirect struct {
 	Code      string `json:"code" bson:"code" msgpack:"code"`
 	URL       string `json:"url" bson:"url" msgpack:"url" validate:"empty=false & format=url`
--- shortener/service.go
+++ shortener/service.go
@@ -1,5 +1,6 @@
 package shortener
 
+//RedirectService dwindows 10 built dools
 type RedirectService interface {
 	Find(code string) (*Redirect, error)
 	Store(redirect *Redirect) error
