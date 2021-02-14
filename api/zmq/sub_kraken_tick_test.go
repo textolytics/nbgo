9468e3c66f768e5c04e0db1eb71064d0631f55bd
textolytics <textolytics@gmail.com>
2021-02-13 22:01:09 -0500

    dfgsd

---
+++ api/zmq/sub_kraken_tick_test.go
@@ -0,0 +1,32 @@
+//
+//  Pubsub envelope subscriber.
+//
+
+package zmq
+
+import (
+	"testing"
+
+	zmq "github.com/pebbe/zmq4"
+
+	"fmt"
+)
+
+//SubKrakenTick PAt Subs
+func TestSubKrakenTick(t *testing.T) {
+	//  Prepare our subscriber
+	subscriber, _ := zmq.NewSocket(zmq.SUB)
+	defer subscriber.Close()
+	subscriber.Connect("tcp://zmq.nb.lan:5558")
+	subscriber.SetSubscribe("")
+
+	for i := 0; i < 1000; i++ {
+		//  Read envelope with address
+		address, _ := subscriber.Recv(0)
+		//  Read message contents
+		contents, _ := subscriber.Recv(0)
+		fmt.Printf("[%s] %s\n", address, contents)
+		t.Error(contents)
+	}
+
+}
